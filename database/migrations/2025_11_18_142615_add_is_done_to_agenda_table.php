05716f5a82608792ba986ab5427752887d9fc178
Ronaa Syarwah <ronaa.syarwah@example.com>
2025-11-18 22:32:32 +0800

    Penyelesaian UTS

---
+++ database/migrations/2025_11_18_142615_add_is_done_to_agenda_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('agenda', function (Blueprint $table) {
+            // Menggunakan tipe ENUM dengan nilai yang diizinkan 'true' atau 'false'
+            // Default diisi 'false'
+            $table->enum('is_done', ['true', 'false'])->default('false')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('agenda', function (Blueprint $table) {
+            $table->dropColumn('is_done');
+        });
+    }
+};
